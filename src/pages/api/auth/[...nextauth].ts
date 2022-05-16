49ea9f0e1bda196ee6f737669d0fa4c18e06e18c
Helder Correia <helder.patrick25@gmail.com>
2022-05-16 20:35:18 +0100

    Save user in Faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,52 @@
+import { query as q } from 'faunadb';
 import NextAuth from 'next-auth'
 import GitHubProvider from "next-auth/providers/github";
 
 
+import { fauna } from '../../../services/fauna';
+
 export default NextAuth({
-  providers: [,
+  providers: [
     GitHubProvider({
         clientId: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET
       })
   ],
+  callbacks: {
+    async signIn(user) {
+      const { email } = user.user;
+      
+      try{
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
+          )
+        )
+
+        return true;
+      } catch(error) {
+        console.log("error")
+        console.log(error)
+        return false;
+      }
+
+    }
+  },
 })
--- src/services/fauna.ts
+++ src/services/fauna.ts
@@ -2,4 +2,6 @@ import { Client } from 'faunadb';
 
 export const fauna = new Client({
   secret: process.env.FAUNA_DB_KEY,
+  domain: 'db.eu.fauna.com',
+  scheme: 'https',
 })
